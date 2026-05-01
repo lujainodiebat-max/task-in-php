febff6e7a3cc0e75fe6bc6333a98f46fc6c17ef1
lujainodiebat-max <lujainodiebat@gmail.com>
2026-05-01 19:05:56 +0300

    Add files via upload

---
+++ task.php
@@ -0,0 +1,56 @@
+<?php
+$fname="lujain muder";
+echo"fullname is  ".fname ;
+$newname = strtoupper(fname);
+echo "the new upper name is: ".$newname;
+echo "length of chars in name: ".strlen($newname);
+replacedn= str_replace(" ", "**", $newname);
+
+
+
+<?php
+$text= "ello World!";
+if(strpos($text,"x")!==false or strpos($text,"b")!==false or strpos($text,"H"==false))
+{
+    echo "found";
+}
+
+else
+{
+    echo "not found";
+}
+?>
+
+<?php
+$name="lujain odiebat ";
+$newname1= strtoupper($name);
+echo $newname1;
+$lename= strlen($newname1);
+echo $lename." ";
+$newst= str_replace(" ", "**" , $newname1);
+echo $newst;
+
+
+?>
+
+
+
+
+
+
+
+
+
+<?php
+$n = rand(10,500);
+echo $n;
+if ($n>50){
+    echo " approved";
+}
+else{
+    echo "not approved";
+}
+
+?>
+
+?>
